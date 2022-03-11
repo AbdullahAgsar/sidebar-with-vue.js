ffe3426767458ec358f704e7f8e8f6cf47d42ab8
AbdullahAgsar <agsar7634@gmail.com>
2022-03-11 18:53:48 +0300

    index.js fixed

--- index.js
+++ index.js
@@ -56,43 +56,4 @@ const sidebar = Vue.createApp({
           })
         }
     }
-    /*
-    methods:{
-        openNextMenu(event){
-            //const allShow = document.querySelectorAll(".show")
-            event.currentTarget.classList.toggle("show");
-        },
-    },
-    
-                {
-                    id:1,
-                    menuName: "Home",
-                    icons : "fas fa-home"
-                },
-                {
-                    id:2,
-                    menuName: "Posts",
-                    icons : "fas fa-comments"
-                },
-                {
-                    id:3,
-                    menuName: "News",
-                    icons : "fas fa-newspaper"
-                },
-                {
-                    id:4,
-                    menuName: "Announcements",
-                    icons : "fas fa-bullhorn"
-                },
-                {
-                    id:5,
-                    menuName: "My Profile",
-                    icons : "fas fa-user"
-                },
-                {
-                    id:6,
-                    menuName: "Settings",
-                    icons : "fas fa-cog"
-                },
-    */
 }).mount('#sidebar');
